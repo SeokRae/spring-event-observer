e3b941e81e6275ca26702d18c959c08f456c561d
SeokRae <kslbsh@gmail.com>
2024-08-22 17:08:56 +0900

    Initial commit

---
+++ src/main/java/com/example/observer/SpringEventObserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventObserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringEventObserverApplication.class, args);
+  }
+
+}
